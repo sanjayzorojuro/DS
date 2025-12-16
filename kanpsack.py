a6600a2b983337bb8c110b4e9cecaecbf8094026
sanjay rao <sanjayraops17@gmail.com>
2025-12-16 17:30:36 +0530

    initial commit

--- kanpsack.py
+++ kanpsack.py
@@ -1,10 +1,10 @@
-def knapsack(n,c,w,p):
+def knapsacks(n,c,w,p):
     if n == 0 or c == 0:
         return 0 
     if w[n-1] > c : 
-        return knapsack(n-1,c,w,p)
+        return knapsacks(n-1,c,w,p)
     else:
-        return max(p[n-1]+knapsack(n-1,c-w[n-1],w,p),knapsack(n-1,c,w,p))
+        return max(p[n-1]+knapsacks(n-1,c-w[n-1],w,p),knapsacks(n-1,c,w,p))
     
 p = [10,20,30,50]
 w = [1,2,3,4]
@@ -17,6 +17,6 @@ print("The weight is:",w)
 print("The capacity is:",c)
 
 print()
-print("Optimal solution:")
-print(knapsack(n,c,w,p))
+print("Optimal solution:")  
+print(knapsacks(n,c,w,p))
 
